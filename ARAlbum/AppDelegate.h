efd57045b2597431397f76197dba52db5ec232c4
俞志云 <qifeiqin@qinqifeidemacbook-air.local>
2018-03-19 10:47:30 +0800

    Initial Commit

---
+++ ARAlbum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARAlbum
+//
+//  Created by 俞志云 on 2018/3/19.
+//  Copyright © 2018年 俞志云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARAlbum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARAlbum
+//
+//  Created by 俞志云 on 2018/3/19.
+//  Copyright © 2018年 俞志云. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end

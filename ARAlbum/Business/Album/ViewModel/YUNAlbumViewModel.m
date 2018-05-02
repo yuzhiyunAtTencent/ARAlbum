946a5bc016a1a00f6bc7a899aa9d2ea41f069b05
yuzhiyunAtTencent <zhiyunyu@tencent.com>
2018-05-02 12:10:34 +0800

    使用AFNetworking访问后台获取列表页数据

--- ARAlbum/Business/Album/ViewModel/YUNAlbumViewModel.m
+++ ARAlbum/Business/Album/ViewModel/YUNAlbumViewModel.m
@@ -7,6 +7,7 @@
 //
 
 #import "YUNAlbumViewModel.h"
+#import <AFNetworking.h>
 
 @interface YUNAlbumViewModel ()
 
@@ -25,6 +26,40 @@
 }
 
 - (void)loadListItemsWithSuccess:(YUNAlbumLoadSuccessBlock)successBlock
+                            fail:(YUNAlbumLoadFailBlock)failBlcok {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://140.143.19.42:8080/arPhotoList"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+            
+            failBlcok();
+        } else {
+            NSLog(@"%@", response);
+            NSLog(@"%@", responseObject);
+            NSArray *resultList = (NSArray*)responseObject;
+            for (NSDictionary *item in resultList) {
+                YUNAlbumPhotoModel *model = [[YUNAlbumPhotoModel alloc] init];
+                model.title = item[@"title"];
+                model.place = item[@"place"];
+                model.date = item[@"date"];
+                model.coverImgUrl = item[@"coverImgUrl"];
+                
+                [self.listItems addObject:model];
+            }
+            
+            successBlock();
+        }
+    }];
+    [dataTask resume];
+}
+
+// 加载本地json文件作为列表
+- (void)loadLocalListItemsWithSuccess:(YUNAlbumLoadSuccessBlock)successBlock
                             fail:(YUNAlbumLoadFailBlock)failBlcok {
     // 获取本地bundle中的json数据
     NSString *path = [[NSBundle mainBundle] pathForResource:@"photoData" ofType:@"json" inDirectory:@"File.bundle"];
